f9c8ed0a1192fd7ee538fccb82db8b07510eebbf
Rene Cocom <fasnimo@gmail.com>
2019-04-05 04:05:56 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 def prime?(integer)
  false if integer < 2 
- (2...integer).each do |solve|
-   if (integer -1 % solve).zero?
+ (2...integer -1).each do |solve|
+   if (integer % solve).zero?
      return false
    end
  end
